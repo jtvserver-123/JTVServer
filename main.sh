50c06fdce9b4062cec5bc75bb1f52b4be5d6d6b6
jtvserver-123 <123730628+jtvserver-123@users.noreply.github.com>
2023-01-27 19:36:00 +0530

    Update main.sh

--- main.sh
+++ main.sh
@@ -5,6 +5,7 @@ echo moved into space
 echo installing JTVServer
 wget https://github.com/avipatilpro/JioTV/archive/refs/heads/main.zip
 unzip main.zip
+ls
 mv /opt/render/project/src/JioTV/main /opt/render/project/src/JioTV/JioTV
 cd ../
 php -S localhost:8080 -t "JioTV"
