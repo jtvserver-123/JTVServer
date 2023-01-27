3bdb16fa4c0bb29ddf35f4a86af68d5a63311526
jtvserver-123 <123730628+jtvserver-123@users.noreply.github.com>
2023-01-27 19:57:20 +0530

    Update main.sh

--- main.sh
+++ main.sh
@@ -1,3 +1,5 @@
+echo installing dependencies
+apt-get install wget php
 echo creating space
 mkdir JioTV
 cd JioTV
@@ -6,10 +8,7 @@ echo installing JTVServer
 wget https://github.com/avipatilpro/JioTV/archive/refs/heads/main.zip
 unzip main.zip
 mv JioTV-main JioTV
-ls
 echo Trying
 cd /opt/render/project/src/JioTV
-ls
-apt install php
 php -S localhost:8080 -t "JioTV"
 echo hosted
